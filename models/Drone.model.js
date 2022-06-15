5b493a3378c187ec1752a5ed0e0615036c354d1a
Guillem <guillem.mart7@gmail.com>
2022-06-15 16:40:45 +0200

    iteration 1 done, drones are included in the db

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,20 @@
 // Iteration #1
+const {Schema, model} = require("mongoose");
+
+const droneSchema = new Schema ({
+    name: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    propellers: {
+        type:Number,
+    },
+    maxSpeed: {
+        type:Number,
+    }
+});
+
+const Drone = model("drone", droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+const mongoose = require ("mongoose");
+const Drone = require ("../models/Drone.model");
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+
+mongoose.connect(MONGO_URI)
+    .then(x => console.log(`Connected to ${x.connection.name}`))
+    .then(() => {
+        return Drone.create()
+    })
+    .catch(err => console.log(err))
+    .finally(() => {
+        mongoose.connection.close();
+    });
